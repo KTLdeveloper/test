a30bd16adc3ebe9274612be3b16f82b5ea964f6f
Davis Lin <ylsh10309@gmail.com>
2020-09-02 15:47:42 +0000

    Initial commit

---
+++ src/controllers/UserController/index.js
@@ -0,0 +1,19 @@
+const { createUser } = require("./create");
+const { readAllUser } = require("./read");
+const { loginUser } = require("./create");
+const { loginpageUser } = require('./create');
+const { signupUser } = require('./create');
+const { signuppageUser } = require('./create');
+const { updateUser } = require('./update');
+const { deleteUser } = require('./delete');
+
+module.exports = {
+    createUser,
+    readAllUser,
+    loginUser,
+    loginpageUser,
+    signupUser,
+    signuppageUser,
+    updateUser,
+    deleteUser
+}
